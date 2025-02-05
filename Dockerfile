d70834795c937af755c8340fea768172fbd14434
Bruno-M44 <bruno.maguin@posteo.net>
2025-02-05 18:00:51 +0100

    implementation upload picture

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,5 @@ COPY . .
 
 EXPOSE 4000
 
-CMD [ "flask", "run", "--host=0.0.0.0", "--port=4000"]
+CMD [ "flask", "run", "--debug", "--host=0.0.0.0", "--port=4000"]
 
--- app/views/picture_view.py
+++ app/views/picture_view.py
@@ -1,12 +1,16 @@
+import os
 import werkzeug.exceptions
-from flask import jsonify, make_response, request, Blueprint
+from flask import jsonify, make_response, request, Blueprint, current_app
 from flask_jwt_extended import jwt_required
 
 from ..models import FamilyTreeCell, Picture
 from ..schemas import pictures_schema, picture_schema
 from .verify_user_authorized import VerifyUserAuthorized
 from app import db
+from werkzeug.utils import secure_filename
+import uuid
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 picture_app = Blueprint("picture_app", __name__)
 
@@ -32,7 +36,8 @@ def create_picture(id_family_tree_cell: int):
     family_tree_cell = FamilyTreeCell.query.get(id_family_tree_cell)
     new_picture = Picture(
         picture_date=request.json.get("picture_date"),
-        comments=request.json.get("comments")
+        comments=request.json.get("comments"),
+        
     )
     family_tree_cell.pictures.append(new_picture)
     db.session.commit()
@@ -97,3 +102,49 @@ def get_update_delete_picture(id_family_tree_cell: int, id_picture: int):
             "data": result
         }
         return make_response(jsonify(data), data["status"])
+
+
+@picture_app.route("/family_trees/<int:id_family_tree>/family_tree_cells/<int:id_family_tree_cell>/pictures", methods=["POST"], endpoint="upload_picture")
+@jwt_required()
+@VerifyUserAuthorized
+def upload_picture(id_family_tree: int, id_family_tree_cell: int):
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    # current_app.logger.info("---allowed_file(file.filename): %s", allowed_file(file.filename))
+    if file and allowed_file(file.filename):
+    # Générer un nom de fichier unique
+        filename = str(uuid.uuid4()) + '.' + file.filename.rsplit('.', 1)[1].lower()
+        filename = secure_filename(filename)
+        
+        os.makedirs(f"/pictures/{id_family_tree}/{id_family_tree_cell}", exist_ok=True)
+        file.save(f"/pictures/{id_family_tree}/{id_family_tree_cell}/{filename}")
+        
+        family_tree_cell = FamilyTreeCell.query.get(id_family_tree_cell)     
+        new_picture = Picture(
+            filename=filename,
+            picture_date=request.form["picture_date"],
+            comments=request.form["comments"],
+            header_picture=request.form["header_picture"]
+        )
+        family_tree_cell.pictures.append(new_picture)
+        db.session.commit()
+        result = picture_schema.dump(new_picture)
+        
+    else:
+        return jsonify({"error": "File not allowed"}), 400
+             
+    data = {
+    "message": "Picture Created !",
+    "status": 201,
+    "data": result
+    }
+    return make_response(jsonify(data), data["status"])
+
+def allowed_file(filename: str):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
