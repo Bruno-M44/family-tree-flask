6bcdb2c8e41c85294002cec0551fd17a7aee4ef0
Bruno-M44 <bruno.maguin@posteo.net>
2026-03-30 10:51:01 +0200

    mode demo

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 from os import environ
 
 SQLALCHEMY_DATABASE_URI = environ.get('DB_URL')
+SQLALCHEMY_ENGINE_OPTIONS = {
+    "pool_size": 5,
+    "max_overflow": 5,
+    "pool_pre_ping": True,
+    "pool_recycle": 300,
+}
